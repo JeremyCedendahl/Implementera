0809f2b6660ecf68bc37f55ae5f9e5350f670fd0
JeremyCedendahl <100521709+JeremyCedendahl@users.noreply.github.com>
2022-07-10 19:21:51 +0200

    Add files via upload

---
+++ Implementera/Labb1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Labb1.Adapter;
+using Labb1.Factory;
+using System;
+using System.Threading;
+
+namespace Labb1
+{
+    class Program
+    {
+       public static void Main()
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapterr(adaptee);
+
+            Singleton.GetInstance().Typer(target.GetRequest());
+            Console.WriteLine();
+
+
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    MakeDish(new BurgerCreator());
+                    break;
+                case "2":
+                    MakeDish(new PizzaCreator());
+                    break;
+
+                default:
+                    break;
+            }
+          
+        }
+   public static void MakeDish(Creator creator)
+        {
+            Singleton.GetInstance().Typer(creator.FoodOperation()); 
+        }
+    }
+}
